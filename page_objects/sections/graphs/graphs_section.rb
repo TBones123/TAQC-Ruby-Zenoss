27474fef04c4def5e336a6b7dc13f1dfa6eeb96f
andrey.radushev <radushev123@gmail.com>
2021-07-13 15:30:27 +0300

    adding a section for searching graphs and information inside a graph

--- page_objects/sections/graphs/graphs_section.rb
+++ page_objects/sections/graphs/graphs_section.rb
@@ -1,8 +1,7 @@
 class GraphsSection < SitePrism::Section
   class GraphSection < SitePrism::Section
     element :name_graph, 'div.x-toolbar div.x-form-display-field'
-    elements :metric_elements, 'div.zenfooter tr.zenfooter_value_row'
-    elements :points_graph, '.nv-linesWrap.nvd3-svg'
+    elements :metric_elements, 'div.zenfooter tr.zenfooter_value_row td.zenfooter_data'
   end
   sections :graphs, GraphSection, 'div.x-container div.x-panel.graph-panel'
 end
